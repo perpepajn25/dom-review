36139f6683bcfd7aa4ca2d6b229cef4ce5affd72
perpepajn25 <perpepajn25@gmail.com>
2018-06-08 14:17:18 -0400

    with example solution

---
+++ example-solution-index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let buttonArea = document.getElementById('buttons')
+  let tableArea = document.getElementById('mult-table')
+  buttonArea.addEventListener('click', handleButtonClick)
+  attachButtons()
+
+
+  function attachButtons () {
+    for (let i = 1; i < 10; i++) {
+      let button = document.createElement('button')
+      button.innerText = `${i}`
+      button.className = 'multiplier'
+      buttonArea.append(button)
+    }
+  }
+
+  function handleButtonClick (event) {
+    if (event.target.className === 'multiplier'){
+      tableArea.innerHTML = ''
+      let number = parseInt(event.target.innerText)
+      generateRows(number)
+    }
+  }
+
+  function generateRows(num) {
+    for (let i = 1; i <= 10; i++) {
+      let newRow = `<tr>
+        <td>
+          ${num} x ${i} = ${num*i}
+        </td>
+      </tr>`
+      tableArea.innerHTML += newRow
+    }
+  }
+
+})
