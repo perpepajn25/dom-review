f86494af53f0145ba31e1f9f71eaebe635a1b30d
perpepajn25 <perpepajn25@gmail.com>
2018-06-08 14:26:14 -0400

    updated upon refresh

--- example-solution-index.js
+++ example-solution-index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let tableArea = document.getElementById('mult-table')
   buttonArea.addEventListener('click', handleButtonClick)
   attachButtons()
+  generateRows(1)
 
 
   function attachButtons () {
